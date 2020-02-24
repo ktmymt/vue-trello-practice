877811b5fcf65386a0b040ebdbe33b8b2765c35c
Kota Miyamoto <kotamiyamoto@KotanoMacBook-Pro.local>
2020-02-24 21:52:44 +0900

    props and style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div id="app" />
+  <div id="app">
+    <List v-for="list in lists" :key="list.id" :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { IList } from "@/types";
+import List from "@/components/List.vue";
 import { createInitialLists } from "@/InitialData.ts";
 
-@Component
+@Component({
+  components: {
+    List
+  }
+})
 export default class App extends Vue {
   lists: IList[] = createInitialLists();
 }
 </script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  > .list {
+    margin: 1px;
+  }
+}
+</style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,31 @@
 <template>
-    <div />
+  <div>
+    {{ list.name }}
+    <Card v-for="card in List.cards" :key="card.id" :card="card" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { IList } from "@/types";
+import Card from "@/components/Card.vue";
 
-@Component
-export default class List extends Vue {}
+@Component({
+  components: {
+    Card
+  }
+})
+export default class List extends Vue {
+  @Prop({ type: Object, required: true })
+  list!: IList;
+}
 </script>
+
+<style lang="scss" scoped>
+.list {
+  border: 1px solid #000000;
+  > .card {
+    margin: 1px;
+  }
+}
+</style>
